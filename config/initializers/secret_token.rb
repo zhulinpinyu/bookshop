5b3e6576a591252ea76c7a97b29a616d22002fb8
mulixiang199016 <mu_lixiang@vanceinfo.com>
2012-08-22 08:47:07 +0800

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookshop::Application.config.secret_token = '200784c6536ba447cfc821ab9088ea3e92cd8641c69ab7756ee717f3cb5f940e2406d49b7d9716bf10a65d8ad3f2de5fa1777facf9db97d9f1716ec09cf6fc35'
