86b2cd48e3b8195e253f44e10334cd653fbf8198
mulixiang199016 <mu_lixiang@vanceinfo.com>
2012-10-15 19:47:04 +0800

    test2

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -12,7 +12,7 @@ class ProductTest < ActiveSupport::TestCase
 
   test "product price must be positive" do
     product = Product.new(title: "mlx",
-    					  description: "mlx",
+    					  description: "mlx", 
     					  image_url: "mlx.jpg")
     product.price=-1
     assert product.invalid?
@@ -25,5 +25,33 @@ class ProductTest < ActiveSupport::TestCase
     product.price=1
     assert product.valid?
  
+  end
+
+  def new_product(image_url)
+    Product.new(title: "mlx",
+                description: "mlx",
+                price:1,
+                image_url: image_url)
+  end
+  test "image_url" do
+    ok = %w{ mlx.gif mlx.jpg mlx.png MLX.JPG MLX.Jpg http://a.b.c/x/y/z/mlx.gif }
+    bad = %w{ mlx.doc mlx.gif/more mlx.gif.more }
+    ok.each do |name|
+        assert new_product(name).valid?, "#{name} should not be invalid"
+    end
+
+    bad.each do |name|
+        assert new_product(name).invalid?, "#{name} should not be valid"
+    end
+  end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(title: products(:ruby).title,
+                          description: "yyy",
+                          price: 1,
+                          image_url: "mlx.png")
+    assert !product.save
+    #assert_equal "has already been taken", product.errors[:title].join(';')
+    assert_equal I18n.translate('activerecord.errors.messages.taken'), product.errors[:title].join(';')
   end
 end
