019481f50c184a6c713d4ffd279e64afe122c399
mulixiang199016 <mu_lixiang@vanceinfo.com>
2012-10-17 10:46:03 +0800

    finish store

--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -4,6 +4,10 @@ class StoreControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '#columns #side a', minimum: 4
+    assert_select '#main .entry', 1
+    assert_select 'h3','ruby 1.9.3'
+    assert_select '.price', /\$[,\d]+\.\d\d/
   end
 
 end
